ae87cffb3e3387722bae6918912f3a342dc021c2
Allison Farris <allison.perry98@gmail.com>
2023-10-04 20:27:56 -0600

    added render function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
-const shapes = require("./lib/shapes")
+const { Circle, Triangle, Square } = require("./lib/shapes")
 
 const questions = [
 {
@@ -30,14 +30,38 @@ const questions = [
 
 // fix writefile function
 function writeTofile(fileName, data) {
-    var renderLogo = renderLogo(data);
-fs.writeFile(fileName, data)
+    //var renderLogo = renderLogo(data);
+fs.writeFile(fileName, data, function(error) {
+    if (error) 
+    return console.log(error);
+})
+console.log("Rendering Logo...")
 };
 
 function init() {
     inquirer.prompt(questions).then(function(data) { 
-        var fileName = "logo.svg";   
-        writeTofile(fileName, data)
+           const {
+            logoText, textColor, logoShape, shapeColor 
+           } = data
+           if (logoShape === "Circle") {
+            const circle = new Circle (logoText, textColor, shapeColor) 
+            circle.render()
+            circle.setcolor(shapeColor);
+            writeTofile("./examples/logo.svg", circle.render()) 
+           } 
+           if (logoShape === "Square") {
+            const square = new Square (logoText, textColor, shapeColor)
+            square.render()
+            square.setcolor(shapeColor);
+            writeTofile("./examples/logo.svg", square.render()) 
+           }
+           if (logoShape === "Triangle") {
+            const triangle = new Triangle (logoText, textColor, shapeColor)
+            triangle.render()
+            triangle.setcolor(shapeColor);
+            writeTofile("./examples/logo.svg", triangle.render())
+           } 
+        
     })
 };
 init();
